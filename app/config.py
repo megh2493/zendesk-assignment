6226cedee532b0beac036170b7d96c10ce384fee
megh2493 <mmadhusu@usc.edu>
2018-03-07 00:23:31 -0800

    Moved some hard coded values to config file and added opening of web browser on run

--- app/config.py
+++ app/config.py
@@ -1,3 +1,6 @@
 import os
 
 SECRET_KEY = os.urandom(24)
+HOST = '127.0.0.1'
+PORT = 5000
+DEBUG = False
--- run.py
+++ run.py
@@ -1,6 +1,24 @@
+import requests
+import webbrowser
+
+from multiprocessing import Process
+
 from app import app
 
-app.debug = True
+
+# waits till server is ready, then opens the login page
+def open_browser(url):
+    while True:
+        try:
+            requests.get(url, timeout=0.5)
+            webbrowser.open(url)
+            return
+        except requests.exceptions.ConnectionError:
+            pass
+
 
 if __name__ == '__main__':
-    app.run()
+    host = app.config.get('HOST', '127.0.0.1')
+    port = app.config.get('PORT', 5000)
+    Process(target=open_browser, args=('http://%s:%s/' % (host, port),)).start()
+    app.run(host=host, port=port) # start the application
