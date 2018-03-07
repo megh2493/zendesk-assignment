508040e19a589e0fb2c67ecedc19097ef3ae32fd
megh2493 <mmadhusu@usc.edu>
2018-03-07 00:22:30 -0800

    Removed an unnecessary import

--- app/models.py
+++ app/models.py
@@ -1,15 +1,15 @@
 import requests
 import requests.compat
 
-from app import app
 
-
+# creates a zendesk session for the user
 def get_zendesk_session(email, password):
     session = requests.Session()
     session.auth = (email, password)
     return session
 
 
+# model to store user details
 class User:
     def __init__(self, domain):
         self.domain = domain
@@ -18,6 +18,10 @@ class User:
         self.session = None
         self.img = None
 
+    def __del__(self):
+        self.session.close()
+
+    # verify login credentials and update user details
     def login(self, email, password):
         self.session = get_zendesk_session(email, password)
         r = self.session.get(requests.compat.urljoin(self.domain, '/api/v2/users/me.json'))
